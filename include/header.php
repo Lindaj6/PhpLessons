03806a2f7045182c88111ce4f35c8645b3453a05
Linda Jiang <linda.jiang@students.jmcss.org>
2024-04-09 08:27:59 -0500

    9d

--- include/header.php
+++ include/header.php
@@ -1,17 +1,10 @@
 
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="style.css">
-  </head>
-  <body>
-    <div class="mynav">
-  <nav class="navbar navbar-expand-lg">
+    <nav class="navbar navbar-expand-lg">
   <div class="container-fluid">
+  <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav">
         <li class="nav-item">
@@ -40,12 +33,10 @@
         <li class="nav-item">
           <a class="nav-link" href="superglobals.php">Superglobals</a>
         </li>
-        </li>
       </ul>
     </div>
   </div>
 </nav>
-</div>
 <?php
 $_SESSION ['username'] = "Jian24Lg";
 echo $_SESSION ['username'];
