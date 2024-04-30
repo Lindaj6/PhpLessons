7e9bd53a80dab79552c11e403e3f977051f6e37c
Linda Jiang <110932278+Lindaj6@users.noreply.github.com>
2024-04-29 22:47:27 -0500

    Update signup.php

--- signup.php
+++ signup.php
@@ -17,30 +17,30 @@ require "include/header.php";
         <h1>Signup</h1>
         <?php
     if (isset($_GET['error'])){
-    if ($_GET['error'] == "emptyfields"){
+    if ($_GET["error"] == "emptyfields"){
         echo '<p class="signuperror">Fill in all fields!</p>';
     }
-else if($_GET['error'] == "invaliduidmail"){
+else if($_GET["error"] == "invaliduidmail"){
     echo '<p class= "signuperror"> Invalid username and e-mail! </p>';
 }
-else if($_GET['error'] == "invaliduid"){
+else if($_GET["error"] == "invaliduid"){
     echo '<p class="signuperror"> Invalid username! </p>';
 }
-else if($_GET['error'] == "invaliduidmail"){
+else if($_GET["error"] == "invalidmail"){
     echo '<p class= "signuperror"> Invalid e-mail! </p>';
 }
-else if($_GET['error'] == "passwordcheck"){
+else if($_GET["error"] == "passwordcheck"){
     echo '<p class="signuperror">Your passwords do not match!</p>';
 }
-else if($_GET['error'] =="usertaken"){
+else if($_GET["error"] == "usertaken"){
     echo '<p class= "signuperror">Username is already taken!</p>';
 }
-else if ($_GET['signup'] =="success"){
+    }
+else if ($_GET["signup"] == "success"){
     echo '<p class="signupsuccess">Signup successful!</p>'
 }
 ?>
-  
-        <form action="include/signup.inc.php" method="post">
+        <form class="form-signup" action="include/signup.inc.php" method="post">
             <input type="text" name="uid" placeholder="Username">
             <input type="text" name="mail" placeholder="E-mail">
             <input type="password" name="pwd" placeholder="Password">
